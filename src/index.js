effa19f1666f29ada0677999a859f34ad694772c
Luciana Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-05-28 08:46:28 -0300

    index comentado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const emailRoutes = require('./routes/emailRoutes');
-const documentRoutes = require('./routes/documentRoutes');
+// Importação dos módulos necessários
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const emailRoutes = require("./routes/emailRoutes");
+const documentRoutes = require("./routes/documentRoutes");
 
+// Criação da aplicação Express
 const app = express();
+
+// Configuração do middleware para parsear o corpo das requisições como JSON
 app.use(express.json());
 
-mongoose.connect('mongodb://mongodb:27017/tcs', { useNewUrlParser: true, useUnifiedTopology: true });
+// Conexão com o banco de dados MongoDB
+mongoose.connect("mongodb://mongodb:27017/tcs", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use('/api', emailRoutes);
-app.use('/api', documentRoutes);
+// Configuração das rotas da aplicação
+app.use("/api", emailRoutes);
+app.use("/api", documentRoutes);
 
+// Inicialização do servidor na porta 3000
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
