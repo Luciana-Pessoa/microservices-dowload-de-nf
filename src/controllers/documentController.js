d7ec0933c9fa5c534f7b124147afb7b1b065377b
Luciana Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-05-28 08:44:41 -0300

    documentcontroller comentado

--- src/controllers/documentController.js
+++ src/controllers/documentController.js
@@ -1,15 +1,25 @@
+// Importação dos módulos necessários
 const Document = require('../models/Document');
 const { parseStringPromise } = require('xml2js');
 
+// Definição da função getInfoDocument que será exportada
 exports.getInfoDocument = async (req, res) => {
+  // Recuperação do nome do arquivo a partir dos parâmetros da requisição
   const { filename } = req.params;
+
+  // Busca do documento no banco de dados pelo nome do arquivo
   const document = await Document.findOne({ filename });
 
+  // Verificação se o documento foi encontrado
   if (!document) {
+    // Se o documento não foi encontrado, retorna um erro 404
     return res.status(404).send('Document not found');
   }
 
+  // Conversão do conteúdo do arquivo de XML para JSON
   const parsedXml = await parseStringPromise(document.contentFile);
+
+  // Extração das informações relevantes do JSON
   const info = {
     cNF: parsedXml.nfeProc.NFe[0].infNFe[0].$.cNF,
     emitCNPJ: parsedXml.nfeProc.NFe[0].infNFe[0].emit[0].CNPJ[0],
@@ -22,5 +32,6 @@ exports.getInfoDocument = async (req, res) => {
     }))
   };
 
+  // Envio das informações extraídas como resposta
   res.json(info);
 };
